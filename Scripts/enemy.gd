ad85295f1666e60f6776c425b08f0915d2f1bfd7
Devdath <devdathpradeepvaliyaveetil@gmail.com>
2026-02-17 01:21:26 +0530

    health working ig, janky as hell tho

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,7 +3,9 @@ extends CharacterBody2D
 @export var speed = 50
 var gravity = 900
 @export var health = 50
+@export var damage = 20
 var DeathExplosionScene = preload("res://Scenes/death_explosion.tscn")
+@onready var player = get_parent().get_node("CharacterBody2D")
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -24,3 +26,7 @@ func take_damage(amount:int) -> void:
 		explosion.global_position = global_position
 		get_tree().current_scene.add_child(explosion)
 		queue_free()
+
+func _on_hit_area_area_entered(area: Area2D) -> void:
+	print("player damaged")
+	player.take_damage(damage)
